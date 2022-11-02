71e788ef0239282fbf372126b922ff7ca6543c7a
mmurilo03 <105663942+mmurilo03@users.noreply.github.com>
2022-11-01 22:16:00 -0300

    Livro Chagas, Baruque & Baruque (2010):

---
+++ CBBAtividadePratica1.java
@@ -0,0 +1,11 @@
+public class CBBAtividadePratica1 {
+    public static void main(String[] args){
+        String frase1;
+        String frase2;
+        frase1 = new String("Testando igualdade!");
+        frase2 = new String("Testando igualdade!");
+        System.out.println("Frase 1: " + frase1);
+        System.out.println("Frase 2: " + frase2);
+        System.out.println("Frase 1 e Frase 2 são igual? R: " + (frase1==frase2));
+    }
+}
