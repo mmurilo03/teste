4ddc8a98c149da7f1c2887c52450f56340a6a3d1
livelton <105663496+livelton@users.noreply.github.com>
2022-11-01 21:31:44 -0300

    Upando

---
+++ BMExercicio04.java
@@ -0,0 +1,10 @@
+public class BMExercicio04 {
+    public static void main(String[] args) {
+        int preço = 100;
+        String marca = "Philips";
+        double potência = 4.5;
+        String tipo = "LED Bulbo";
+
+        System.out.println("Lâmpada " + marca + " " + tipo + "\nPor " + preço + " Com potência de " + potência + "W");
+    }
+}
